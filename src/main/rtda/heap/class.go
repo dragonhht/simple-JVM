1debef70dd1cb7ac30f51c011cdbe961d99f1fc8
dragonhht <dragon_hht@163.com>
2020-10-16 20:42:50 +0800

    加载器

--- src/main/rtda/heap/class.go
+++ src/main/rtda/heap/class.go
@@ -1,5 +1,7 @@
 package heap
 
+import "main/classfile"
+
 type Class struct {
 	accessFlags uint16
 	name string
@@ -14,4 +16,16 @@ type Class struct {
 	instanceSlotCount uint
 	staticSlotCount uint
 	staticVars *Slots
+}
+
+func newClass(cf *classfile.ClassFile) *Class {
+	class := &Class{}
+	class.accessFlags = cf.AccessFlags()
+	class.name = cf.ClassName()
+	class.superClassName = cf.SuperClassName()
+	class.interfaceNames = cf.InterfaceNames()
+	class.constantPool = newConstantPool(class, cf.ConstantPool())
+	class.fields = newFields(class, cf.Fields())
+	class.methods = newMethods(class, cf.Methods())
+	return class
 }
---
+++ src/main/rtda/heap/class_loader.go
@@ -0,0 +1,90 @@
+package heap
+
+import (
+	"fmt"
+	"main/classfile"
+	"main/classpath"
+)
+
+type ClassLoader struct {
+	cp *classpath.Classpath
+	classMap map[string]*Class
+}
+
+func NewClassLoader(cp *classpath.Classpath) *ClassLoader {
+	return &ClassLoader{
+		cp:       cp,
+		classMap: make(map[string]*Class),
+	}
+}
+
+func (self *ClassLoader) LoadClass(name string) *Class {
+	if class, ok := self.classMap[name]; ok {
+		return class
+	}
+	return self.loadNonArray(name)
+}
+
+func (self *ClassLoader) loadNonArray(name string) *Class {
+	// 读取class文件
+	data, entry := self.readClass(name)
+	// 解析class文件
+	class := self.defineClass(data)
+	link(class)
+	fmt.Printf("[Loaded %s from %s]\n", name, entry)
+	return class
+}
+
+func link(class *Class) {
+	verify(class)
+	prepare(class)
+}
+
+func prepare(class *Class) {
+	// TODO 准备阶段处理
+}
+
+func verify(class *Class) {
+	// TODO 验证
+}
+
+func (self *ClassLoader) readClass(name string) ([]byte, classpath.Entry) {
+	data, entry, err := self.cp.ReadClass(name)
+	if err != nil {
+		panic("java.lang.ClassNotFoundException: " + name)
+	}
+	return data, entry
+}
+
+func (self *ClassLoader) defineClass(data []byte) *Class {
+	class := parseClass(data)
+	class.loader = self
+	resolveSuperClass(class)
+	resolveInterfaces(class)
+	self.classMap[class.name] = class
+	return class
+}
+
+func resolveInterfaces(class *Class) {
+	interfaceCount := len(class.interfaceNames)
+	if interfaceCount > 0 {
+		class.interfaces = make([]*Class, interfaceCount)
+		for i, interfaceName := range class.interfaceNames {
+			class.interfaces[i] = class.loader.LoadClass(interfaceName)
+		}
+	}
+}
+
+func resolveSuperClass(class *Class) {
+	if class.name != "java/lang/Object" {
+		class.superClass = class.loader.LoadClass(class.superClassName)
+	}
+}
+
+func parseClass(data []byte) *Class {
+	cf, err := classfile.Parse(data)
+	if err != nil {
+		panic("java.lang.ClassFormatError")
+	}
+	return newClass(cf)
+}
